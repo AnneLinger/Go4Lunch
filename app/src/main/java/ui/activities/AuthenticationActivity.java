1bca5b66f50801ad985cdffd70485a0ee937e91b
Anne <anne-linger@orange.fr>
2022-06-23 10:05:34 +0200

    Initial commit

---
+++ app/src/main/java/ui/activities/AuthenticationActivity.java
@@ -0,0 +1,31 @@
+package ui.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.anne.linger.go4lunch.databinding.ActivityAuthenticationBinding;
+
+/**
+*Activity for the authentication of the user
+*/
+public class AuthenticationActivity extends AppCompatActivity {
+
+    //For UI
+    private ActivityAuthenticationBinding mBinding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initUi();
+    }
+
+    //Configure the UI
+    private void initUi() {
+        mBinding = ActivityAuthenticationBinding.inflate(getLayoutInflater());
+    }
+}
+
+
