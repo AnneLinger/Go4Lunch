6a1c722ec43d202bda80960c6b390892d4f6775c
Anne <anne-linger@orange.fr>
2022-07-05 15:17:49 +0200

    Display a google map view

--- app/src/main/java/ui/fragments/MapViewFragment.java
+++ app/src/main/java/ui/fragments/MapViewFragment.java
@@ -9,22 +9,45 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.anne.linger.go4lunch.R;
 import com.anne.linger.go4lunch.databinding.FragmentMapViewBinding;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
 *Fragment to display a map for places
 */
-public class MapViewFragment extends Fragment {
+public class MapViewFragment extends Fragment implements OnMapReadyCallback {
 
     //For UI
     private FragmentMapViewBinding mBinding;
 
     //For data
 
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        assert supportMapFragment != null;
+        supportMapFragment.getMapAsync(this);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = FragmentMapViewBinding.inflate(getLayoutInflater());
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+       googleMap.addMarker(new MarkerOptions()
+               .position(new LatLng(0, 0))
+               .title("Marker"));
     }
 }
