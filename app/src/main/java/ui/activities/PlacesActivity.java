6d92525e1397a783ff251df27f47524ccc57b966
Anne <anne-linger@orange.fr>
2022-07-07 10:31:18 +0200

    Ask permission for location

--- app/src/main/java/ui/activities/PlacesActivity.java
+++ app/src/main/java/ui/activities/PlacesActivity.java
@@ -1,34 +1,47 @@
 package ui.activities;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.Window;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.anne.linger.go4lunch.R;
 import com.anne.linger.go4lunch.databinding.ActivityPlacesBinding;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.List;
+
 import dagger.hilt.android.AndroidEntryPoint;
+import kotlin.contracts.SimpleEffect;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
 import ui.fragments.ListViewFragment;
 import ui.fragments.MapViewFragment;
 import ui.fragments.WorkmatesFragment;
 import viewmodel.UserViewModel;
 
 /**
-*Main activity of the app
+*Main activity of the app launched after login
 */
 
 @AndroidEntryPoint
-public class PlacesActivity extends AppCompatActivity {
+public class PlacesActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
     //For UI
     private ActivityPlacesBinding mBinding;
@@ -38,6 +51,14 @@ public class PlacesActivity extends AppCompatActivity {
     private UserViewModel mUserViewModel;
     private final AuthenticationActivity mAuthenticationActivity = new AuthenticationActivity();
 
+    //For permission
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 124;
+    private boolean permissionDenied = false;
+    private GoogleMap mGoogleMap;
+    private static final int DEFINE_LOCATION_REQUEST_CODE = 1;
+    private String fineLocation = Manifest.permission.ACCESS_FINE_LOCATION;
+    private String coarseLocation = Manifest.permission.ACCESS_COARSE_LOCATION;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,12 +67,23 @@ public class PlacesActivity extends AppCompatActivity {
         showSnackBar(getString(R.string.successful_auth));
         configureBottomNav();
         mBinding.bottomNav.setSelectedItemId(R.id.item_map_view);
+        checkUserLocation();
     }
 
     //Configure the UI
     private void initUi() {
         mBinding = ActivityPlacesBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+    }
+
+    //Configure data
+    private void configureViewModel() {
+        mUserViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+    }
+
+    // Show Snack Bar with a message
+    private void showSnackBar( String message){
+        Snackbar.make(mBinding.getRoot(), message, Snackbar.LENGTH_SHORT).show();
     }
 
     //Configure bottom nav
@@ -74,19 +106,45 @@ public class PlacesActivity extends AppCompatActivity {
         return false;
     }
 
-    //Configure data
-    private void configureViewModel() {
-        mUserViewModel = new ViewModelProvider(this).get(UserViewModel.class);
-    }
-
-    // Show Snack Bar with a message
-    private void showSnackBar( String message){
-        Snackbar.make(mBinding.getRoot(), message, Snackbar.LENGTH_SHORT).show();
-    }
-
-    //TODO : configure in the layout
+    //TODO : configure in the drawer
     //To log out
     private void logOut() {
         mUserViewModel.logOut();
+    }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(DEFINE_LOCATION_REQUEST_CODE)
+    private void checkUserLocation() {
+        if (EasyPermissions.hasPermissions(this, fineLocation) || EasyPermissions.hasPermissions(this, coarseLocation)) {
+            //mGoogleMap.setMyLocationEnabled(true);
+        }
+        else {
+            requestPermission();
+        }
+    }
+
+    private void requestPermission() {
+        EasyPermissions.requestPermissions(this, String.valueOf(R.string.rationale_location), LOCATION_PERMISSION_REQUEST_CODE, fineLocation, coarseLocation);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+        else {
+            requestPermission();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 }
