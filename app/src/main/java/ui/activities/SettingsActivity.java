41b3e0bdb0d1e70ccced01bdde013e1a663a6a2f
Anne <anne-linger@orange.fr>
2022-07-09 10:31:47 +0200

    Settings activity layout

--- app/src/main/java/ui/activities/SettingsActivity.java
+++ app/src/main/java/ui/activities/SettingsActivity.java
@@ -1,14 +1,18 @@
 package ui.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.anne.linger.go4lunch.databinding.ActivityPlacesBinding;
 import com.anne.linger.go4lunch.databinding.ActivitySettingsBinding;
+
+import java.util.Objects;
 
 /**
 *Activity for the user settings
@@ -24,11 +28,27 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initUi();
+        configureActionBar();
     }
 
     //Configure UI
     private void initUi() {
         mBinding = ActivitySettingsBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+    }
+
+    private void configureActionBar() {
+        mBinding.settingsToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToPlacesActivity();
+            }
+        });
+    }
+
+    private void navigateToPlacesActivity() {
+        Intent intent = new Intent(SettingsActivity.this, PlacesActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
