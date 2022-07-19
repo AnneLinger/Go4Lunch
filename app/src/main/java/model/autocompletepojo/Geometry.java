eef0fcd6cdd5f6f0c61a9ba3c43485b9ddba1dbb
Anne <anne-linger@orange.fr>
2022-07-19 11:02:11 +0200

    Add pojo models for place autocomplete

---
+++ app/src/main/java/model/autocompletepojo/Geometry.java
@@ -0,0 +1,39 @@
+package model.autocompletepojo;
+
+import android.gesture.Prediction;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ *Pojo model for geometry autocomplete
+*/
+
+public class Geometry {
+
+    @SerializedName("predictions")
+    @Expose
+    private List<Prediction> predictions = null;
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    public List<Prediction> getPredictions() {
+        return predictions;
+    }
+
+    public void setPredictions(List<Prediction> predictions) {
+        this.predictions = predictions;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ app/src/main/java/model/autocompletepojo/MainTextMatchedSubstring.java
@@ -0,0 +1,35 @@
+package model.autocompletepojo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ *Pojo model for main text matched substring autocomplete
+*/
+
+public class MainTextMatchedSubstring {
+
+    @SerializedName("length")
+    @Expose
+    private Integer length;
+
+    @SerializedName("offset")
+    @Expose
+    private Integer offset;
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+}
