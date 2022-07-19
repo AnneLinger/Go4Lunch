fe04e4d845b59adb53f2776d4d884391d7bb6dc3
Anne <anne-linger@orange.fr>
2022-07-19 10:45:15 +0200

    Add pojo models for place details

---
+++ app/src/main/java/model/placedetailpojo/Geometry.java
@@ -0,0 +1,40 @@
+package model.placedetailpojo;
+
+import android.location.Location;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import model.nearbysearchpojo.Viewport;
+
+/**
+ *Pojo model for geometry place details
+*/
+
+public class Geometry {
+
+    @SerializedName("location")
+    @Expose
+    private Location location;
+
+    @SerializedName("viewport")
+    @Expose
+    private Viewport viewport;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Viewport getViewport() {
+        return viewport;
+    }
+
+    public void setViewport(Viewport viewport) {
+        this.viewport = viewport;
+    }
+
+}
